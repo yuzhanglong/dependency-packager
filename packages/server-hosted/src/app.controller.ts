42f518b7359630818d31aee8a272943e7ecb6047
yuzhanglong <loveyzl1123@gmail.com>
2023-04-02 05:10:18 +0800

    feat: add server host api

--- packages/server-hosted/src/app.controller.ts
+++ packages/server-hosted/src/app.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Req, Request } from '@nestjs/common';
-import { DependencyPackager } from '@yzl/dependency-packager';
+import { DependencyPackager, PackageInfo } from '@yzl/dependency-packager';
 
 @Controller('/')
 export class AppController {
@@ -7,11 +7,24 @@ export class AppController {
 
   @Get('package-dependency/*')
   async packageDependency(@Req() request: Request) {
-    const packageParts = request.url.replace('/package-dependency/', '').split('@');
-    console.log(packageParts);
-    return this.dependencyPackager.packageDependency({
-      name: 'react',
-      version: 'latest',
-    });
+    const packageURL = request.url.replace('/package-dependency/', '');
+    const packageParts = packageURL.split('@');
+
+    const packageInfo: PackageInfo = { name: '', version: '' };
+    if (packageParts.length === 3) {
+      // 形如 @babel/runtime@version 格式
+      const [, name, version] = packageParts;
+      packageInfo.name = `@${name}`;
+      packageInfo.version = version;
+    } else if (packageParts.length === 2) {
+      // 形如 react@version 格式
+      const [name, version] = packageParts;
+      packageInfo.name = name;
+      packageInfo.version = version;
+    } else {
+      throw new Error('Invalid package name');
+    }
+
+    return this.dependencyPackager.packageDependency(packageInfo);
   }
 }
